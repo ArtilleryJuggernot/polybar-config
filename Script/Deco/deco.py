a0da6f92d3cf420b46bd51e7466a14a3c703b91e
MrHugojuggernot <hugojuggernot@gmail.com>
2022-05-10 21:35:24 +0200

    Update config file with the needed script

---
+++ Script/Deco/deco.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Python Script for stdout data through config file
+
+import configparser
+import sys
+from os.path import expanduser
+
+config = configparser.ConfigParser()
+
+# Open file and  parsing
+with open(expanduser('deco-config'), 'r', encoding='utf-8') as f:
+    config.read_file(f)
+
+
+data  = [x for x in config.sections()]
+
+# Display element
+for element  in currencies:
+    c = config[element]['content']
+    sys.stdout.write(f'{c}')
